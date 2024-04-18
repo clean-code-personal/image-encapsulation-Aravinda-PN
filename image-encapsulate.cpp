9bcb96321bb8b4621b43d19ab9aece2147cbae52
PN, Aravinda <Aravinda.Nagendra@philips.com>
2024-04-18 12:57:36 +0530

    addressing comments

--- image-encapsulate.cpp
+++ image-encapsulate.cpp
@@ -3,9 +3,10 @@
 
 int main() {
 	auto image = std::make_shared<Image>(512, 512);
-	std::cout << "Brightening a 512 x 512 image\n";
-	ImageBrightener brightener(image);
+	
 	if (image->isValid()) {
+		ImageBrightener brightener(image);
+		std::cout << "Brightening a 512 x 512 image\n";
 		int attenuatedCount = brightener.BrightenWholeImage();
 		std::cout << "Attenuated " << attenuatedCount << " pixels\n";
 		std::cout << "... in an image of: " << image->m_rows << " x " << image->m_columns << "\n";
